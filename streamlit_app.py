87e3b41b75253cd5d1d76f96fc9c2024fe1b158c
Rigved Sarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2025-03-10 11:06:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,18 +8,15 @@ from datetime import datetime
 # Display Title and Description
 st.title("Biolume Skin Science Sales Management System")
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+# Your app code goes here
 
 # Constants
 SALES_SHEET_COLUMNS = [
